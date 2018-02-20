142a3b6ff1e3d30ac7b01c6691e65417ce945909
usuario <usuario@simu6.ujaen.es>
2018-02-20 11:54:46 +0100

    Meter cookie y tiempo de sesion

--- GitHub1/src/paq/Servlet1.java
+++ GitHub1/src/paq/Servlet1.java
@@ -1,41 +1,40 @@
 package paq;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class Servlet1
- */
+
 @WebServlet("/Servlet1")
+
+
+
 public class Servlet1 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+HttpSession sesion = request.getSession(true);
+response.setContentType("text/html");
+PrintWriter html = response.getWriter();
+html.print("<strong>");
+Date date = (Date)sesion.getAttribute("date");
+if(date != null) {
+html.print("Último acceso de la sesión: " + date + "<br>");
+}
+else {
+html.print("Este es el primer acceso de la sesión <br>");
+}
+date = new Date();
+sesion.setAttribute("date", date);
+sesion.setMaxInactiveInterval(5);
+html.print("Fecha actual: " + date);
+html.print("</strong>");
+}
 
 }
